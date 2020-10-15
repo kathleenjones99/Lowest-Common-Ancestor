73f8b548f319e6717c08fae5550c5db94f4855b4
kathleenjones99 <jonesk7@tcd.ie>
2020-10-15 16:31:48 +0100

    New LCA implementation using binary tree class, method taken from geeksforgeeks

---
+++ src/lca/MyBinaryTree.java
@@ -0,0 +1,77 @@
+// Kathleen Jones 17325867
+// BinaryTree and Node structures taken from geeksforgeeks, find code at: 
+// https://www.geeksforgeeks.org/binary-tree-set-1-introduction/
+// LCA of Binary Tree implementation also taken from geeksforgeeks, find code at
+
+package lca;
+
+public class MyBinaryTree {
+
+	// Root of Binary Tree 
+    Node root; 
+  
+    // Constructors 
+    MyBinaryTree(int key) 
+    { 
+        root = new Node(key); 
+    } 
+  
+    MyBinaryTree() 
+    { 
+        root = null; 
+    } 
+  
+    private Node findLCA(int node1, int node2) {
+    	return findLCA(root, node1, node2);
+    }
+    
+    private Node findLCA(Node node, int node1, int node2) {
+    	
+    	Node LCA = null;
+    	
+    	if(node == null) {											// base case, tree/node passed is null
+    		return LCA;
+    	} else if (node.key == node1 || node.key == node2) {		// If either n1 or n2 matches with root's key, report 
+            LCA = node;  											// the presence by returning root (Note that if a key is 
+            return LCA;
+    	}    			   											// ancestor of other, then the ancestor key becomes LCA)
+    	
+        Node left_lca = findLCA(node.left, node1, node2); 			// Look for keys in left and right subtrees 
+        Node right_lca = findLCA(node.right, node1, node2); 
+  
+        // If both of the above calls return Non-NULL, then one key 
+        // is present in one subtree and other is present in other, 
+        // so this node is the LCA.
+        // Otherwise check if left subtree or right subtree is LCA 
+        if (left_lca!=null && right_lca!=null) { 
+            LCA = node; 
+        } else {													
+        	LCA = (left_lca != null) ? left_lca : right_lca; 
+        }
+        
+    	return LCA;
+    }
+    
+    
+    // main function building a binary tree and using function to find LCA
+    public static void main(String args[]) 
+    { 
+        MyBinaryTree tree = new MyBinaryTree(); 
+        tree.root = new Node(1); 
+        tree.root.left = new Node(2); 
+        tree.root.right = new Node(3); 
+        tree.root.left.left = new Node(4); 
+        tree.root.left.right = new Node(5); 
+        tree.root.right.left = new Node(6); 
+        tree.root.right.right = new Node(7); 
+        System.out.println("LCA(4, 5) = " + 
+                            tree.findLCA(4, 5).key); 
+        System.out.println("LCA(4, 6) = " + 
+                            tree.findLCA(4, 6).key); 
+        System.out.println("LCA(3, 4) = " + 
+                            tree.findLCA(3, 4).key); 
+        System.out.println("LCA(2, 4) = " + 
+                            tree.findLCA(2, 4).key); 
+    } 
+    
+} 
---
+++ src/lca/Node.java
@@ -0,0 +1,15 @@
+// geeksforgeeks: https://www.geeksforgeeks.org/binary-tree-set-1-introduction/
+package lca;
+
+public class Node {
+	
+	int key; 
+    Node left, right; 
+  
+    public Node(int item) 
+    { 
+        key = item; 
+        left = right = null; 
+    }
+    
+}
