b3dfa2d49cc5bab2b2e2fc76c673ea660ee437fd
kathleenjones99 <jonesk7@tcd.ie>
2020-10-21 09:44:10 +0100

    adding junit test class

---
+++ src/lca/TestMyBinaryTree.java
@@ -0,0 +1,14 @@
+package lca;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMyBinaryTree {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
