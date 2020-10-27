119390a0dca86ebd476992e771eb2cb462575a24
kathleenjones99 <jonesk7@tcd.ie>
2020-10-27 11:43:24 +0000

    Tests added for JUnit test class to test findLCA implementation

--- src/lca/TestMyBinaryTree.java
+++ src/lca/TestMyBinaryTree.java
@@ -7,8 +7,30 @@ import org.junit.Test;
 public class TestMyBinaryTree {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void findLCAOfEmptyTree() {
+		MyBinaryTree tree = new MyBinaryTree(); 
+		assertNull("If LCA is called on an empty tree, retur null", tree.findLCA(1, 2));
 	}
-
+	
+	@Test
+	public void findLCAOfTreeWithOneNode() {
+		MyBinaryTree tree = new MyBinaryTree();
+		tree.root = new Node(1);
+		assertEquals("Where a binary tree has only one node which is the root, the LCA of the root passed twice will be the root", tree.findLCA(1, 1), tree.root);
+	}
+	
+	@Test
+	public void findLCAOfNonEmptyTree() {
+		MyBinaryTree tree = new MyBinaryTree(); 
+        tree.root = new Node(1); 
+        tree.root.left = new Node(2); 
+        tree.root.right = new Node(3); 
+        tree.root.left.left = new Node(4); 
+        tree.root.left.right = new Node(5); 
+        tree.root.right.left = new Node(6); 
+        tree.root.right.right = new Node(7); 
+        
+        assertEquals("Given a non-empty binary tree, the lowest common ancestor should be the lowest node in the tree that has both passed nodes as a descendent", tree.findLCA(4, 5), tree.root.left);
+        assertEquals("Given a non-empty binary tree, the lowest common ancestor should be the lowest node in the tree that has both passed nodes as a descendent", tree.findLCA(2, 7), tree.root);       
+	}
 }
